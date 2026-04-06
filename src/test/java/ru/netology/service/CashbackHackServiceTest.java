1624a582640e76bd90c76a4296cbb585f444c622
Atenza767 <atenza767@gmail.com>
2026-04-06 10:13:28 +0200

    initial commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,20 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    @Test
+   public void remain() {
+
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 900;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 100;
+
+        assertEquals(expected, actual);
+    }
+}
