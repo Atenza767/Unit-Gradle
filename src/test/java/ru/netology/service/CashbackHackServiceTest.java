53b54ef0e95618bd7e6e975ded4eb110b8ba2700
Atenza767 <atenza767@gmail.com>
2026-04-12 19:12:54 +0200

    falled commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -10,10 +10,10 @@ class CashbackHackServiceTest {
    public void remain() {
 
         CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 900;
+        int amount = 1000;
 
         int actual = cashbackHackService.remain(amount);
-        int expected = 100;
+        int expected = 0;
 
         assertEquals(expected, actual);
     }
